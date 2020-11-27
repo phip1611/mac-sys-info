6c2eb16b05d58adc1a55c5bc2c03a1884244dec1
Philipp Schuster <phip1611@gmail.com>
2020-11-27 12:46:57 +0100

    Binary with simple CLI works

---
+++ src/main.rs
@@ -0,0 +1,83 @@
+//! Binary for the library.
+
+use mac_sys_info::get_mac_sys_info;
+use std::process::exit;
+use mac_sys_info::structs::mac_sysinfo::MacSysInfo;
+
+enum Action {
+    ShowHelp,
+    ShowSimple,
+    ShowComplex,
+}
+
+fn main() {
+    let info = get_mac_sys_info();
+    if info.is_err() {
+        // IDE may does not find the Display trait impl, but its there
+        // it gets implemented during compilation by "derive_more" crate
+        eprintln!("There was an error. Can't get system information. Error: {}", info.unwrap_err());
+        exit(-1);
+    }
+    let info = info.unwrap();
+
+    let action = parse_args();
+    match action {
+        Action::ShowHelp => { print_help() }
+        Action::ShowSimple => { print_simple(&info) }
+        Action::ShowComplex => { print_full(&info) }
+    }
+
+}
+
+fn parse_args() -> Action {
+    let args: Vec<String> = std::env::args()
+        .map(|s| s.to_lowercase())
+        .collect();
+    if args.len() == 1 {
+        return Action::ShowSimple;
+    };
+
+    if args[1] == "-h" || args[1] == "--help" || args[1] == "help" || args[1] == "h" {
+        return Action::ShowHelp;
+    }
+
+    if args[1] == "-c" {
+        return Action::ShowComplex;
+    }
+
+    Action::ShowSimple
+}
+
+fn print_simple(info: &MacSysInfo) {
+    println!("CPU:");
+    println!("    name     : {}", info.cpu_info().brand_string());
+    // IDE may does not find the Display trait impl, but its there
+    // it gets implemented during compilation by "derive_more" crate
+    println!("    arch     : {}", info.cpu_info().architecture());
+    println!("    frequency: {} @ GHz", info.cpu_info().frequency_ghz());
+    println!("    L1D-Cache: {}KB", info.cpu_info().cache_info().l1d_cache_size_kb());
+    println!("    L1I-Cache: {}KB", info.cpu_info().cache_info().l1i_cache_size_kb());
+    println!("    L2-Cache : {}KB", info.cpu_info().cache_info().l2_cache_size_kb());
+    println!("    L3-Cache : {}KB", info.cpu_info().cache_info().l3_cache_size_kb());
+    println!();
+    println!("Memory:");
+    println!("    total: {}MB", info.mem_info().total_memory_mb());
+    println!();
+    println!("OS:");
+    println!("    version: {}", info.os_info().os_version());
+    println!("    kernel : {}", info.os_info().kern_version());
+}
+
+fn print_help() {
+    println!("Simple output : '$ mac-sys-info'");
+    println!("Complex output: '$ mac-sys-info -c'");
+    println!("Help          : '$ mac-sys-info -h'");
+}
+
+fn print_full(info: &MacSysInfo) {
+    println!("{}", info.cpu_info());
+    println!("{}", info.cpu_info().cache_info());
+    println!("{}", info.mem_info());
+    println!("{}", info.os_info());
+    println!("{}", info.cpu_features());
+}
